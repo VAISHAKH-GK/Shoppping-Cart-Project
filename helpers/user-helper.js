64119928981d38cbf606c6e53aa51c05a1e887c8
Vaishakh GK <vaishakhgk2006@gmail.com>
2021-09-12 10:51:46 +0530

    Button issue fix login on signup

--- helpers/user-helper.js
+++ helpers/user-helper.js
@@ -4,38 +4,41 @@ const collection = require('../config/collection');
 module.exports = {
     doSignup: (userData) => {
         return new Promise((resolve, reject) => {
+            let responce = {};
             bcrypt.genSalt().then((salt) => {
                 bcrypt.hash(userData.Password, salt).then((hash) => {
                     userData.Password = hash;
-                    userData.Password_2 = hash;
                     db.get().collection(collection.user).insertOne(userData).then((data) => {
-                        resolve(data.Password);
+                        loginst = true;
+                        responce.user = userData;
+                        responce.status = true;
+                        resolve(responce);
                     });
                 });
             });
         });
 
-    },doLogin:(userDatal)=>{
-        return new Promise(async(resolve,reject)=>{
-            let loginst=false;
+    }, doLogin: (userDatal) => {
+        return new Promise(async (resolve, reject) => {
+            let loginst = false;
             let responce = {};
-            let mail=await db.get().collection(collection.user).findOne({Email:userDatal.Email});
-            if(mail){
-                bcrypt.compare(userDatal.Password,mail.Password).then((status)=>{
-                    if(status){
+            let mail = await db.get().collection(collection.user).findOne({ Email: userDatal.Email });
+            if (mail) {
+                bcrypt.compare(userDatal.Password, mail.Password).then((status) => {
+                    if (status) {
                         console.log('logged in');
-                        loginst=true;
-                        responce.user=mail;
-                        responce.status=true;
-                        resolve(responce);       
-                    }else{
+                        loginst = true;
+                        responce.user = mail;
+                        responce.status = true;
+                        resolve(responce);
+                    } else {
                         console.log('wrong password');
-                        resolve({status:false});
+                        resolve({ status: false });
                     }
                 });
-            }else{
+            } else {
                 console.log('wrong email');
-                resolve({status:false});
+                resolve({ status: false });
             }
         });
     }
